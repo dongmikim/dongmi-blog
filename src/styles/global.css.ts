b3f65a39b901fb4f5408c46ff8c1798fc58f31a8
dongmikim <dongmi.kim@gmail.com>
2024-11-10 17:10:59 +0900

    chore: hr style

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,5 +1,5 @@
 import { globalStyle } from '@vanilla-extract/css'
-import { themeContract } from './themes.css'
+import { themeContract, themes } from './themes.css'
 
 globalStyle(
   'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video',
@@ -34,8 +34,8 @@ globalStyle('a', {
 globalStyle('ol, ul', { listStyle: 'none' })
 
 globalStyle('hr', {
-  marginTop: 100,
-  marginBottom: 100,
+  marginTop: 42,
+  marginBottom: 42,
 })
 
 globalStyle('img', {
